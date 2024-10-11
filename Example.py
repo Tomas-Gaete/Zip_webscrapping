81f9b8e4ab8f35c5cf5eb2626d6d8e623eb76a6a
Tomas-Gaete <tgaeteb@gmail.com>
2024-10-11 11:54:11 -0300

    Python file now downloads the desired zip file. TODO make the date selected automatically update to current day

--- Example.py
+++ Example.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+
 
 # Define the URL
-url = "https://www.google.com"
-
+#url = "https://www.coordinador.cl/"
+url = "https://www.coordinador.cl/operacion/documentos/programas-de-operacion-2021/"
 # Initialize the Chrome driver (adjust the executable path if needed)
 driver = webdriver.Chrome()  # Or provide the path to chromedriver if not in PATH
 
@@ -13,13 +15,19 @@ driver.get(url)
 # Wait for the page to load completely (optional, useful for dynamic content)
 driver.implicitly_wait(5)
 
-# Perform a search as an example
-search_box = driver.find_element(By.NAME, 'q')
-search_box.send_keys('Selenium Web Scraping')  # Example search
-search_box.submit()
+date = "11 de Octubre, 2024"
+read_more = driver.find_element(By.CSS_SELECTOR, "#readMore_btn")
+read_more.click()
+#Gets download link for zip file
+download_link = driver.find_element(By.XPATH, "//span[contains(text(), 'Programa de Operación y Lista de Prioridades')]/following::span[contains(text(), '11 de Octubre, 2024')]/following::a[contains(@href, '.zip')]")
+zip_url = download_link.get_attribute('href')
+print(f"ZIP file download URL: {zip_url}")
 
-# Print the current URL after search
-print(f"Current URL after search: {driver.current_url}")
 
+#start the download
+download_link.click()
+
+#wait for the download to finish before closing the browser
+time.sleep(10)
 # Close the browser
 driver.quit()
